c2939d93435d39822faef3c3d61cbfdaa2d86997
Rui <47619020+CandiceD17@users.noreply.github.com>
2025-05-05 23:17:06 -0700

    Create 547. Number of Provinces.py

---
+++ 2025/547. Number of Provinces.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(i: int):
+            for j in range(cities):
+                if isConnected[i][j] == 1 and j not in visited:
+                    visited.add(j)
+                    dfs(j)
+        
+        cities = len(isConnected)
+        visited = set()
+        provinces = 0
+
+        for i in range(cities):
+            if i not in visited:
+                dfs(i)
+                provinces += 1
+        
+        return provinces
